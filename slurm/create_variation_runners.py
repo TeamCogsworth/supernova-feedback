04392e75ef73c37bcab44b9c04c9029bae8037bf
Tom Wagg <tomjwagg@gmail.com>
2024-08-07 18:49:04 -0400

    update slurm runners

--- slurm/create_variation_runners.py
+++ slurm/create_variation_runners.py
@@ -19,7 +19,7 @@ python /mnt/home/twagg/supernova-feedback/src/variations.py -f "NAMEHERE.h5" -FL
 
 for name, flag, value in zip(["ce-0.1", "ce-10.0", "beta-0.0", "beta-0.5", "beta-1.0", "ecsn-265", "no-fallback"],
                              ['c', 'c', 'b', 'b', 'b', 'e', 'k'],
-                             ['1.0', '10.0', '0.0', '0.5', '1.0', '-265', '3']):
+                             ['0.1', '10.0', '0.0', '0.5', '1.0', '-265', '3']):
     with open(f"variation-{name}.slurm", "w") as f:
         write_this = basic.replace("NAMEHERE", name).replace("FLAGHERE", flag).replace("VALHERE", str(value))
         f.write(write_this)
