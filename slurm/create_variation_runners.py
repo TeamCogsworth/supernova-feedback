266ed4d25556a3eb72a695a554285e281c806265
Tom Wagg <tomjwagg@gmail.com>
2024-08-09 10:42:59 -0700

    add qcrit to variation runners

--- slurm/create_variation_runners.py
+++ slurm/create_variation_runners.py
@@ -4,7 +4,7 @@ basic = """#!/bin/bash
 #SBATCH --partition=cca
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=128
-#SBATCH --time=1:30:00
+#SBATCH --time=1:10:00
 #SBATCH -o /mnt/home/twagg/supernova-feedback/slurm/logs/NAMEHERE_%A.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=tomwagg@uw.edu
@@ -17,9 +17,10 @@ echo "NAMEHERE"
 
 python /mnt/home/twagg/supernova-feedback/src/variations.py -f "NAMEHERE.h5" -FLAGHERE VALHERE"""
 
-for name, flag, value in zip(["ce-0.1", "ce-10.0", "beta-0.0", "beta-0.5", "beta-1.0", "ecsn-265", "no-fallback"],
-                             ['c', 'c', 'b', 'b', 'b', 'e', 'k'],
-                             ['0.1', '10.0', '0.0', '0.5', '1.0', '-265', '3']):
+for name, flag, value in zip(["ce-0.1", "ce-10.0", "beta-0.0", "beta-0.5", "beta-1.0", "ecsn-265", "no-fallback",
+                              'qcritB-0.0', 'qcritB-1000.0'],
+                             ['c', 'c', 'b', 'b', 'b', 'e', 'k', 'q', 'q'],
+                             ['0.1', '10.0', '0.0', '0.5', '1.0', '-265', '3', '0.0', '1000.0']):
     with open(f"variation-{name}.slurm", "w") as f:
         write_this = basic.replace("NAMEHERE", name).replace("FLAGHERE", flag).replace("VALHERE", str(value))
         f.write(write_this)
