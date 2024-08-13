65502a0a02cc4c0b2e666071c36cef89430d31b2
Tom Wagg <tomjwagg@gmail.com>
2024-08-12 21:56:21 -0700

    porb typo

--- slurm/create_initC_variation_runners.py
+++ slurm/create_initC_variation_runners.py
@@ -17,9 +17,9 @@ echo "NAMEHERE"
 
 python /mnt/home/twagg/supernova-feedback/src/feedback_simulation.py -p 128 -t 200 -f "NAMEHERE.h5" -FLAGHERE VALHERE"""
 
-for name, flag, value in zip(["porb-minus1", "porb-plus1", "q-minus1", "q-plus1"],
+for name, flag, value in zip(["porb-minus1", "porb-0", "q-minus1", "q-plus1"],
                              ['P', 'P', 'q', 'q'],
-                             ['-0.999', '1', '-0.999', '1']):
+                             ['-0.999', '0', '-0.999', '1']):
     with open(f"variation-initC-{name}.slurm", "w") as f:
         write_this = basic.replace("NAMEHERE", name).replace("FLAGHERE", flag).replace("VALHERE", str(value))
         f.write(write_this)
